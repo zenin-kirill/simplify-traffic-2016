e9ae1e574766f7463df05b22fdbb5d7a25bb9fba
Kirill <kerill.net@gmail.com>
2016-09-04 23:09:22 +0300

    Добавлена страница добавления агенств

---
+++ app/pages/agency-manage/components/agency_manage.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+	moduleId: module.id,
+    selector: 'agencies-manage',
+    templateUrl: 'agencies-manage.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class AgenciesManageComponent {}
